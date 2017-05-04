cd4bd46fa5579647d9e34163e4843be1ac7ebad8
Ariel <ariel.m.little@gmail.com>
2017-05-03 21:25:22 -0400

    initial push

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+// Import the ORM
+var orm = require("../config/orm.js");
+
+var burger = {
+    // Select all burgers from burgers table in burgers_db
+    selectAll: function(cb) {
+        console.log('called burger.selectall');
+        orm.selectAll('burgers', function(res) {
+            cb(res);
+        });
+    },
+    // Add new burger to burgers table in burgers_db.
+    insertOne: function(burger_type, cb) {
+        orm.insertOne(burger_type, function(res) {
+            cb(res);
+        });
+    },
+    // Update burger to devoured from burgers table in burgers_db
+    updateOne: function(id, cb) {
+        orm.updateOne(id, function(res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (burgerController.js).
+module.exports = burger;
